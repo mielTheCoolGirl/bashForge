53aaf133269d266f49f82c24ec9a508aeb98033f
mielTheCoolGirl <23miel23@gmail.com>
2025-11-09 01:30:42 +0200

    finishing ls -l flag!

--- bashForge/main.cpp
+++ bashForge/main.cpp
@@ -22,11 +22,45 @@ std::string pwd()
 	return "";
 }
 
-std::string premissionsToString(bool isDir,std::filesystem::path path)
+//Getting the number of hard links per path
+int getNumberOfLinks(std::string path,bool isDir)
+{
+	HANDLE hForFile;
+	if(!(isDir))
+		hForFile=CreateFileW(std::wstring(path.begin(),path.end()).c_str(),GENERIC_READ,FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,nullptr,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,nullptr );
+	else
+		hForFile = CreateFileW(std::wstring(path.begin(), path.end()).c_str(), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, nullptr, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, nullptr);
+	if (hForFile == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "SYSTEM ERROR WHILE FETCHING LINKS\n\n";
+		return -1;
+	}
+
+	BY_HANDLE_FILE_INFORMATION info;
+	//fetch number of hard links
+	if (GetFileInformationByHandle(hForFile, &info))
+	{
+		CloseHandle(hForFile);
+		return info.nNumberOfLinks;
+	}
+		
+	return -1;
+}
+
+std::string timeToString(std::filesystem::path path)
+{
+	auto time = std::filesystem::last_write_time(path);
+	auto timePoint = std::chrono::time_point_cast<std::chrono::system_clock::duration>(time - std::filesystem::file_time_type::clock::now() + std::chrono::system_clock::now());
+	std::time_t fullTime = std::chrono::system_clock::to_time_t(timePoint);
+	std::ostringstream stringTime;
+	stringTime << std::put_time(std::localtime(&fullTime), "%b %d %Y");
+	return stringTime.str();
+}
+std::string premissionsToString(bool isDir, std::filesystem::path path)
 {
 	std::filesystem::perms perm = std::filesystem::status(path).permissions();
 	std::string premissions;
-	premissions += (isDir == 0 ? "d" : "-");
+	premissions += (isDir == 1 ? "d" : "-");
 	//check if the premission of the user (owner/group/others) is read/write/exec, if so add wanted flag 
 	//---------OWNER PREMISSIONS--------
 	if ((perm & std::filesystem::perms::owner_read) != std::filesystem::perms::none)
@@ -74,24 +108,14 @@ std::string premissionsToString(bool isDir,std::filesystem::path path)
 		premissions += "x";
 	else
 		premissions += "-";
-	//-----TODO: FIX LINK COUNT LATER
-	premissions += (isDir == 0 ? " 2 " : " 1 "); //if its dir add placeholder 2 else add placeholder 1 
+
+	premissions += " "+(isDir==1 ? std::to_string(getNumberOfLinks(path.string(),true)) : std::to_string(getNumberOfLinks(path.string(), false)))+" "; //getting number of hard links of file/dir
 
 	EnvironmentUsers user(path.string());
-	premissions += user.getOwner() + " " + user.getGroup()+" ";
-	
-	premissions += std::to_string(std::filesystem::file_size(path))+" ";
-	return premissions;
-}
+	premissions += user.getOwner() + " " + user.getGroup() + " ";
 
-std::string timeToString(std::filesystem::path path)
-{
-	auto time = std::filesystem::last_write_time(path);
-	auto timePoint = std::chrono::time_point_cast<std::chrono::system_clock::duration>(time - std::filesystem::file_time_type::clock::now() + std::chrono::system_clock::now());
-	std::time_t fullTime = std::chrono::system_clock::to_time_t(timePoint);
-	std::ostringstream stringTime;
-	stringTime << std::put_time(std::localtime(&fullTime), "%b %d %Y");
-	return stringTime.str();
+	premissions += std::to_string(std::filesystem::file_size(path)) + " ";
+	return premissions;
 }
 
 std::string totalAmountInPathKB(std::string path)
@@ -101,6 +125,8 @@ std::string totalAmountInPathKB(std::string path)
 		sumOfBytes += std::filesystem::file_size(entry.path());
 	return std::to_string(sumOfBytes / 1000);
 }
+
+
 void ls(char flag)
 {
 	std::string path = pwd();
@@ -127,13 +153,13 @@ void ls(char flag)
 
 char parseFlag(std::string input)
 {
-	return input[4];
+	return input[4]; //currently hardcoded, fix later to find flag after -
 }
 
 void analyse_input(std::string input)
 {
 	char flag;
-	if (input.length() > 3)//TODO: LATER CHECKS WILL NOT CHECKY BY LENGTH BUT BY A MAP OF COMMANDS
+	if (input.length() > 3)//TODO: LATER CHECKS WILL NOT CHECK BY LENGTH BUT BY A MAP OF COMMANDS
 	{
 		flag = parseFlag(input);
 	}
@@ -147,6 +173,7 @@ void analyse_input(std::string input)
 
 
 }
+
 int main()
 {
 
