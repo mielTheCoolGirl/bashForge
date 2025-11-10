72f10a974a9123911857034d014f22103eed8da7
mielTheCoolGirl <23miel23@gmail.com>
2025-11-10 22:31:53 +0200

    updating flag logic

--- bashForge/main.cpp
+++ bashForge/main.cpp
@@ -41,59 +41,59 @@ void ls(std::string flag)
 		for (const auto& entry : std::filesystem::directory_iterator(path))
 			if (entry.path().filename().string().front() != '.')
 				std::cout << entry.path().filename().string() << std::endl;
+		return;
 	}
 	bool longListing = false, humanReadable = false, allFiles = false , recursive=false,reverse=false,timeSort=false;
 	for (char part : flag)
 	{
 		switch (part)
 		{
-			case 'l':
-			{/*
-				std::cout << "Total " << totalAmountInPathKB(path) << "\n";
-				for (const auto& entry : std::filesystem::directory_iterator(path))
-				{
-					FileData currFile(entry.path(), 'l');
-					std::cout << currFile.collectFilePremissions();
-				}
-				*/
-				longListing = true;
-				break;
-			}
-			case 'h':
-			{
-				humanReadable = true;
-				break;
-			}
-			
-			case 'R':
-			{
-				recursive = true;
-				break;
-			}
-			case 'r':
-			{
-				reverse = true;
-				break;
-			}
-			case 't':
-			{
-				timeSort = true;
-				break;
-			}
-			default:
-			{
-				std::cout << "Error in flag naming, please check for typos\n";
-				return;
-			}
-		}
-		if (humanReadable && longListing)
+		case 'l': longListing = true; break;
+
+		case 'h':	humanReadable = true; break;
+
+		case 'R':recursive = true; break;
+
+		case 'r': reverse = true; break;
+
+		case 't':timeSort = true; break;
+
+		default:
 		{
-			std::cout << "Total " << totalAmountInPathKB(path) << "\n";
-				for (const auto& entry : std::filesystem::directory_iterator(path))
-				{
-					FileData currFile(entry.path(), 'h');
-					std::cout << currFile.collectFilePremissions();
-				}
+			std::cout << "Error in flag naming, please check for typos\n";
+			return;
+		}
+		}
+	}
+	std::vector<std::filesystem::directory_entry> entries;
+	for (const auto& entry : std::filesystem::directory_iterator(path))
+	{
+		if (entry.path().filename().string().front() != '.')
+		{
+			entries.push_back(entry);
+		}
+	}
+	if (reverse)
+		std::reverse(entries.begin(), entries.end());
+	if (longListing)
+		std::cout << "Total " << totalAmountInPathKB(path) << "\n";
+	for (const auto& entry : entries)
+	{
+		char flag = '-';
+		if (longListing)
+			flag = 'l';
+		if (humanReadable)
+			flag = 'h';
+
+		if (flag == 'l' || flag == 'h')
+		{
+			FileData currFile(entry.path(), flag);
+			std::cout << currFile.collectFilePremissions();
+		}
+		else
+		{
+			if (entry.path().filename().string().front() != '.')
+				std::cout << entry.path().filename().string() << std::endl;
 		}
 	}
 
@@ -110,25 +110,24 @@ void whoami()
 
 std::string parseFlag(std::string input)
 {
-	return input.substr(input.find("-") + 1); ; //currently hardcoded, fix later to find flag after -
+	bool findFlag = input.find("-")!=input.npos;
+	if (!findFlag)
+		return "";
+	return input.substr(input.find("-") + 1);
 }
 
 void analyse_input(std::string input)
 {
 	std::string flag;
-	if (input.length() > 3)//TODO: LATER CHECKS WILL NOT CHECK BY LENGTH BUT BY A MAP OF COMMANDS
-	{
-		flag = parseFlag(input);
-	}
-	else
-		flag = '-';
-	
+	flag = parseFlag(input);
 	if (input.find("pwd") != std::string::npos)
 		std::cout << pwd() << "\n";
-	if (input.find("ls") != std::string::npos)
+	else if (input.find("ls") != std::string::npos)
 		ls(flag);
-	if (!(input.compare("whoami")))
+	else if (!(input.compare("whoami")))
 		whoami();
+	else
+		std::cout << "error, invalid command\n";
 
 }
 
